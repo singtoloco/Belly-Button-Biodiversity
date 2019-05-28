6899cf05895dfa93947fe8844c71dfa4f7bbbc17
singtoloco <vanich.vacharussiriyuth@gmail.com>
2019-05-28 13:17:04 -0500

    Add gauge chart

--- static/js/backup files/app - Copy.js
+++ static/js/backup files/app - Copy.js
@@ -28,65 +28,6 @@ function buildMetadata(sample) {
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
-    // Enter a speed between 0 and 180
-    var level = data.WFREQ * 180/9;
-    // console.log(`WREQ: ${data.WFREQ}`);
-    // console.log(`level(180): ${level}`);
-
-    // Trig to calc meter point
-    var degrees = 180 - level,
-        radius = .5;
-    var radians = degrees * Math.PI / 180;
-    var x = radius * Math.cos(radians);
-    var y = radius * Math.sin(radians);
-
-    // Path: may have to change to create a better triangle
-    var mainPath = 'M -.0 -0.025 L .0 0.025 L ',
-        pathX = String(x),
-        space = ' ',
-        pathY = String(y),
-        pathEnd = ' Z';
-    var path = mainPath.concat(pathX,space,pathY,pathEnd);
-
-    var data = [{ type: 'scatter',
-      x: [0], y:[0],
-        marker: {size: 28, color:'850000'},
-        showlegend: false,
-        name: 'WFREQ', //what is this for?
-        text: level,
-        hoverinfo: 'text+name'},
-      { values: [50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50/9, 50],
-      rotation: 90,
-      text: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-      textinfo: 'text',
-      textposition:'inside',
-      marker: {colors:['#006400','#11740c','#27841f','#3a9431','#4fa446','#66b45d','#7ec276','#9ad093','#d3d3d3','rgba(255, 255, 255, 0)']},
-      labels: ['8-9', '7-8', '6-7', '5-6', '4-5', '3-4', '2-3', '1-2', '0-1', ''],
-      hoverinfo: 'label',
-      hole: .5,
-      type: 'pie',
-      showlegend: false
-    }];
-
-    var layout = {
-      shapes:[{
-          type: 'path',
-          path: path,
-          fillcolor: '850000',
-          line: {
-            color: '850000'
-          }
-        }],
-      title: '<b>Belly Button Washing Frequency</b> <br> Scrubs per Week',
-      height: 500,
-      width: 500,
-      xaxis: {zeroline:false, showticklabels:false,
-                showgrid: false, range: [-1, 1]},
-      yaxis: {zeroline:false, showticklabels:false,
-                showgrid: false, range: [-1, 1]}
-    };
-
-    Plotly.newPlot('gauge', data, layout);
   })
 }
 
